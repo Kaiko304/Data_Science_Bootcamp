05b8af3960a6ee0103b5fe218e096709846ff066
Kaiko304 <20010304kaiko@gmail.com>
2024-01-01 16:21:46 +0100

    initial upload of projects, done during Data Science Bootcamp (6.10.2023 - 18.02.2024): hangman, eniac, gans and moosic

--- environ/snowmans8_con.py
+++ environ/snowmans8_con.py
@@ -2,5 +2,5 @@ schema= "gans3"
 port= "3306"
 host= "snowmans8.c6udojwcjdps.eu-central-1.rds.amazonaws.com"
 user= "admin"
-password = "8snowFamX"
+password = "ECHTJETZTODERß"
 snowcon = f'mysql+pymysql://{user}:{password}@{host}:{port}/{schema}'
